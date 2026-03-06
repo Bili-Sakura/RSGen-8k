77b104918b39945485a3f9cf1d7b11f4221f1f5e
Bili-Sakura <bili_sakura@zju.edu.cn>
2026-03-06 13:25:11 +0800

    remove

--- hf_space/src/rsgen8k/__init__.py
+++
@@ -1,3 +0,0 @@
-"""RSGen-8k: Remote Sensing Image Generation Scaling to 8K Resolution."""
-
-__version__ = "0.1.0"
--- hf_space/src/rsgen8k/data/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Data loading utilities for remote sensing text prompts."""
-
-from rsgen8k.data.xlrs_bench import load_xlrs_bench_prompts
-
-__all__ = ["load_xlrs_bench_prompts"]
